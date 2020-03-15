abf7fab18a87c3967752645d0fc701c3f302c2ff
gautamrwx <gautam.404@protonmail.com>
2020-03-16 05:26:04 +0530

    TryCatch Prog

---
+++ TryCatchExample.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class TryCatchExample
+{
+    public static void main(String[] args)
+    {
+        int a,x,y,z;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 3 Numbers : ");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        z = sc.nextInt();
+
+        try
+        {
+            // Any Arithemetic Error will be forwarded to catch block
+            a = x/(y-z);
+            System.out.println("A = "+a);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            System.out.println("==> Finally Blocks Executed After All <==");
+        }
+    }
+}
